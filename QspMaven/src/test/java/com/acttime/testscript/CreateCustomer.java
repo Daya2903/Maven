0459a193ba55badebee89a0426d129fe71ffbd8e
jagad <jagad@DESKTOP-MOKRNGC>
2023-06-16 06:40:23 +0530

    Commit1

---
+++ QspMaven/src/test/java/com/acttime/testscript/CreateCustomer.java
@@ -0,0 +1,11 @@
+package com.acttime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	@Test
+	public void testCreateCustomer() {
+		Reporter.log("testCreateCustomer", true);
+	}
+}
