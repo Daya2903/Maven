5fffc188b77e83fe022a2f61cbd2b98d0c0e1da6
jagad <jagad@DESKTOP-MOKRNGC>
2024-03-25 13:23:01 +0530

    Fire

--- QspMaven/src/test/java/com/acttime/testscript/CreateCustomer.java
+++ QspMaven/src/test/java/com/acttime/testscript/CreateCustomer.java
@@ -2,15 +2,17 @@ package com.acttime.testscript;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CreateCustomer {
 	@Test
 	public void testCreateCustomer() {
-		WebDriver d=new ChromeDriver();
+		WebDriver d=new FirefoxDriver();
 		d.get("https://www.facebook.com");
 		System.out.println(d.getTitle());
+		d.quit();
 		Reporter.log("testCreateCustomer", true);
 	}
 }
